9ebd366727b55d87a753760e0a6d24fbafbabbb5
Syed Azam Hassan <kashif.hassan@gmail.com>
2019-06-30 19:28:12 +0500

    Alphabets Dictionary

--- alphabet_dictionary.py
+++ alphabet_dictionary.py
@@ -3,3 +3,7 @@
 # i.e. {1 = 'A', 2 = 'B'.................26 = 'Z'}
 
 print(dict(zip([n for n in range(1,27)], [chr(c) for c in range(65,91)])))
+# or
+print({i+1:chr(i+65) for i in range(26)})
+
+input()
